ce27fb0c5e02f7f8f29bc05d02fc989c3512fd55
mac <mac@macs-MacBook-Air.local>
2021-12-22 10:48:51 +0530

    remove unwanted import

--- src/utils/wallet/Connectors.js
+++ src/utils/wallet/Connectors.js
@@ -1,4 +1,3 @@
-import Web3 from "web3";
 import { TorusConnector } from "@web3-react/torus-connector";
 import { PortisConnector } from "@web3-react/portis-connector";
 import { InjectedConnector } from "@web3-react/injected-connector";
