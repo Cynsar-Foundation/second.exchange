2025efe996507b1a09c88ce981059bbabae8800c
Maek Twain <saransh@muellners.org>
2021-12-29 19:35:31 +0530

    Update Home.jsx

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -10,17 +10,17 @@ export const Home = () => {
         <div className="Home">
             <div className="MainContent">
             <div className="subtitle">
-                DECENTRALIZED BLOGGING AND NFT PLATFORM
+                Create your community, voice collective and share wealth, <p> The first exchange was only benefitting few , lets reclaim the space of our attention and re-distribute what is ours (all of us) </p>
             </div>
             <div className="title-line">
-                Publish blogs,<br />
-                Buy and sell as NFTs,<br />
-                Support your favorite creators.
+                Create communities,<br />
+                Share values,<br />
+                Support your favorite creators, <br/>
             </div>
             <div className="HomeImages">
                 <img className="HomeImages__item" src={CanvasPic} alt="collaborate on canvas" />
                 <img className="HomeImages__item" src={BloggingPic} alt="write and publish blogs" />
-                <img className="HomeImages__item" src={NFTPic} alt="trade NFTs" />
+                <img className="HomeImages__item" src={NFTPic} alt="trade attention" />
             </div>
             <div className="ProfileContainer">
                 <CgProfile size={42} />
