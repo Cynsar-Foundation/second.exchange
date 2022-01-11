5d403c5048eb2e6412f94738c32c7b42071abec8
Shubham <shubham@cynsar.capital>
2022-01-11 14:25:11 +0530

    Adding actions for relay context

--- src/context/nostr/RelayContext.tsx
+++ src/context/nostr/RelayContext.tsx
@@ -2,6 +2,7 @@ import { relayPool } from 'nostr-tools'
 import { FC, useReducer } from 'react'
 import { createContext } from 'vm'
 import { RelayAction, RelayState } from './NostrTypes'
+import { pool } from './pool'
 
 
 const initialRelayState = {
@@ -12,10 +13,18 @@ const RelayContext = createContext(initialRelayState)
 
 export const RelayProvider: FC = ({ children }) => {
     const [state, dispatch] = useReducer((state: RelayState, action: RelayAction) => {
-        const currentState = { ...state };
         switch (action.type) {
+            case 'addRelay':
+                return state.pool.addRelay(action.url, { read: true, write: true })
+            case 'removeRelay':
+                return state.pool.removeRelay(action.url)
+            case 'subSingle':
+                return state.pool.sub({ cb: action.cb, filter: action.filter })
+            case 'subBulk':
+                return state.pool.sub({ cb: action.cb, filter: action.filter })
+
             default:
-                return currentState
+                return state
         }
     }, initialRelayState)
 
