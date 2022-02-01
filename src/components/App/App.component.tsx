18bd2245af710d64991d40d883366850da49e7d5
PulkitVyasCynsar <pulkit@muellners.org>
2022-02-02 00:43:01 +0530

    Publish content to relays

--- src/components/App/App.component.tsx
+++ src/components/App/App.component.tsx
@@ -11,12 +11,17 @@ import HeaderNav from '../Header/Header.component';
 import MainContent from '../MainContent/MainContent.component';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Writer } from '../Writer/Writer';
+import { Writer } from '../Writer/Writer.component';
 import { ArticleView } from '../ArticleView/ArticleView.component';
 import { nostrEventState } from 'src/application/states/relay.state';
+import { sessionKeyState } from 'src/application/state';
 
 function useApp() {
   const [nostrEventsValue, setNostrEventsValue] = useRecoilState(nostrEventState);
+  const sessionKey = useRecoilValue(sessionKeyState);
+  // @ts-ignore
+  const parsedSessionKey = JSON.parse(sessionKey)
+
   useEffect(() => {
     const test: AppConfig = inject('config');
     const relayService: RelayService = inject('relayservice');
@@ -35,6 +40,7 @@ function useApp() {
         authors: [
           '3cc926bad81f4128b7c5d08e49a1025e0120d32b79285fd3f9b70fa2404992e5',
           '7b0ba10b13233979d17e545d56b1c1f6563ce0c9b0d1f3691b5ad3bf3cced6c0',
+          parsedSessionKey['pubKey']
         ],
         kinds: [0, 1, 3],
       },
--- src/components/ArticleView/ArticleView.component.tsx
+++ src/components/ArticleView/ArticleView.component.tsx
@@ -1,11 +1,11 @@
 import './ArticleView.style.scss';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from "react-router-dom"
 import ReactMarkdown from 'react-markdown'
 import { useRecoilValue } from 'recoil';
 
-import ArticleService from 'src/shared/ArticleService';
+// import ArticleService from 'src/shared/ArticleService';
 import { darkModeState } from '../../shared/GlobalState';
 import { nostrEventState } from 'src/application/states/relay.state';
 import { NostrEvent } from 'src/application/interfaces';
@@ -22,32 +22,49 @@ function formatDate(dateIsoString: any) {
 
 export const ArticleView: React.FC = () => {
   const isDarkModeEnabled = useRecoilValue(darkModeState);
+  const nostrArticles = useRecoilValue(nostrEventState);
+  const [article, setArticle] = useState<any>();
+  const [articleLoaded, setArticleLoaded] = useState(false);
+
   const getRouteSlug = () => {
     const location = useLocation();
     if (location.pathname.indexOf("article") >= 0)
       //@ts-ignore
-      return Number(/[^/]*$/.exec(location.pathname)[0]);
+      return /[^/]*$/.exec(location.pathname)[0];
   }
 
-  const getArticle = async (id: number | undefined) => {
+  var temp: any = null;
+  const getArticle = (id: string | undefined) => {
     if (id === undefined)
       return
-    await ArticleService.getById(id)
-      .then((result) => {
-        setArticle(result);
-        setArticleLoaded(true);
-        return result;
-      });
+    // await ArticleService.getById(id)
+    //   .then((result) => {
+    //     setArticle(result);
+    //     setArticleLoaded(true);
+    //     return result;
+    //   });
+    var result = nostrArticles.filter(articleObj => {
+      return articleObj.id === id
+    });
+    // @ts-ignore
+    if(nostrArticles[0]) 
+      console.log(nostrArticles[0].id)
+    temp = result;
   }
 
-  const [article, setArticle] = useState<any>();
-  const [articleLoaded, setArticleLoaded] = useState(false);
-  getArticle(getRouteSlug());
+  useEffect(() => {
+    if(temp !== null)
+    {  setArticle(temp);
+    setArticleLoaded(true)};
+  }, [temp])
+
+getArticle(getRouteSlug())
 
   return(
       <div className="article-view">
             {articleLoaded && 
               <div className="article-view__container">
+                {console.log(article)}
                 {/* @ts-ignore */}
                 <h1 className="article-view__title" style={{ color: (isDarkModeEnabled ? "white" : "black")}}>{article.title}</h1>
                 {/* @ts-ignore */}
