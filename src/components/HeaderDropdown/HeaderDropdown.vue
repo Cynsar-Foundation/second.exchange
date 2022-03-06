4bef5f0916b0dec953e9094b8cba83de5566187d
PulkitVyasCynsar <pulkit@muellners.org>
2022-03-06 12:57:26 +0530

    Remove unused files, fix key dialog opening issue

--- src/components/HeaderDropdown/HeaderDropdown.vue
+++ src/components/HeaderDropdown/HeaderDropdown.vue
@@ -22,7 +22,7 @@
         </q-item>
         <q-item v-close-popup clickable @click="onItemClick">
           <q-item-section>
-            <div class="dropdown__item">
+            <div class="dropdown__item" @click="keysDialog = true">
               <Icon icon="bxs:key" font-size="22px" />
               <span class="dropdown__item-text">Your Keys</span>
             </div>
@@ -30,6 +30,39 @@
         </q-item>
       </q-list>
     </q-btn-dropdown>
+    <q-dialog v-model="keysDialog">
+      <q-card class="px-4 py-2">
+        <q-card-section>
+          <div class="text-lg text-bold tracking-wide leading-relaxed py-2">
+            Your keys <q-icon name="vpn_key" />
+          </div>
+          <p v-if="$store.state.keys.priv">
+            Make sure you back up your private key!
+          </p>
+          <p v-else>Your private key is not here!</p>
+          <div class="mt-1 text-xs">
+            Posts are published using your private key. Others can see your
+            posts or follow you using only your public key.
+          </div>
+        </q-card-section>
+
+        <q-card-section>
+          <p>Private Key:</p>
+          <q-input
+            v-model="$store.state.keys.priv"
+            class="mb-2"
+            readonly
+            filled
+          />
+          <p>Public Key:</p>
+          <q-input v-model="$store.state.keys.pub" readonly filled />
+        </q-card-section>
+
+        <q-card-actions align="right" class="text-primary">
+          <q-btn v-close-popup flat label="Close" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </div>
 </template>
 
@@ -38,11 +71,13 @@ import './HeaderDropdown.scss'
 import { Icon } from '@iconify/vue'
 import {LocalStorage} from 'quasar'
 import {eraseDatabase} from '../../db'
+// import {KeysModal} from 'src/components/KeysModal/KeysModal.vue'
 
 export default {
     name: 'HeaderDropdown',
     components: {
-      Icon
+      Icon,
+      // KeysModal
     },
     setup () {
     return {
@@ -57,6 +92,9 @@ export default {
     }
   },
   methods: {
+    openKeyModal() {
+      this.$refs.key_modal.show()
+    },
     async hardReset() {
       this.$q
         .dialog({
