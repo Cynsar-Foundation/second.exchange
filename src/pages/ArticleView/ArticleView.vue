e8e13efc447f2b51d8097060e64d8c73203cd35e
PulkitVyasCynsar <pulkit@muellners.org>
2022-03-09 10:05:00 +0530

    Fix ArticleView alignment

--- src/pages/ArticleView/ArticleView.vue
+++ src/pages/ArticleView/ArticleView.vue
@@ -8,14 +8,59 @@
 
 <script>
 import './ArticleView.scss'
-// import VueMarkdown from 'vue-markdown-render'
-
 export default {
   name: 'ArticleView',
   components: {
   },
   setup() {
-    const article = '# About The Project'
+    const article = `# second.exchange
+Forked from https://github.com/fiatjaf/branle
+## Dev Setup
+
+### Install the dependencies
+
+### Start the app in development mode (hot-code reloading, error reporting, etc.)
+
+### Build the app for production
+
+
+## The first exchange was not good ,this one is really good
+
+Second exchange is an experimental social media service that lets anyone, anywhere in the world to create content. Second Exchange uses peer-to-peer technology to operate with no central authority, Second Exchange Core is the name of open source software which enables the use of this service.
+
+1. Create a blog post/content or collections that is stored on client side only and is realyed using nostr.
+2. Follow people and content
+3. Group content
+4. Feature in future like adding tips , votes, and more in pipeline
+
+## Developer
+
+We are looking for amazing front end developers who can contribute to this great way of sharing information and knowledge that points back to the users.
+
+## UI
+
+Works as usual , the code of UI @TODO please add dev docs here
+
+## Contribute back
+
+👋 Get started contributing with a good first issue
+
+Before explaining how you can contribute, It's worth mentioning that we use develop branch as the default one. This makes it easier for us to have all the latest, development changes without affecting the current production version. Though, We don't use release branches to make the process a little bit easier !
+
+Pushing to develop branch via PR deploys changes to testing environments(currently only rinkeby).
+Pushing to master branch via PR from develop deploys changes to production servers(mainnet).
+Pushing to master|develop directly will be restricted to make sure the PR's are reviewed.
+Creating PRs end up in relation to develop by default.
+Don't be shy to contribute even the smallest tweak. Everyone will be especially nice and helpful to beginners to help you get started!
+
+## Security Audit
+
+The code in this repository has not been audited.
+
+## Deployed Instance
+
+Coming soon
+`
     return {
       article
     }
