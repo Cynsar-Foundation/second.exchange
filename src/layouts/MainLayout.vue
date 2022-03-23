060fcb443ccd38026fd516f39d698bf861d6e560
PulkitVyasCynsar <pulkit@muellners.org>
2022-03-23 13:14:27 +0530

    Add user post button and list display

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -13,8 +13,6 @@
 </template>
 <script>
 import helpersMixin from '../utils/mixin'
-import {validateWords} from 'nostr-tools/nip06'
-import {generatePrivateKey} from 'nostr-tools'
 import Header from '../components/Header/Header.vue'
 import {pool} from '../pool'
 import {addToThread} from '../utils/threads'
@@ -36,22 +34,6 @@ export default {
     }
   },
 
-  computed: {
-    icon() {
-      return document.getElementById('icon').href
-    },
-
-    isKeyKey() {
-      if (this.isKey(this.key)) return true
-      return false
-    },
-
-    isKeyValid() {
-      if (this.isKeyKey) return true
-      if (validateWords(this.key?.toLowerCase())) return true
-      return false
-    }
-  },
   mounted() {
     this.$store.dispatch('initialize')
 
@@ -85,78 +67,5 @@ export default {
       'profile-browser'
     )
   },
-  methods: {
-    async getFromExtension() {
-      try {
-        this.key = await window.nostr.getPublicKey()
-        this.watchOnly = true
-      } catch (err) {
-        this.$q.notify({
-          message: `Failed to get a public key from a Nostr extension: ${err}`,
-          color: 'warning'
-        })
-      }
-    },
-
-    generate() {
-      this.key = generatePrivateKey()
-      this.watchOnly = false
-    },
-
-    proceed() {
-      let key = this.key?.toLowerCase()
-
-      var keys = {}
-      if (validateWords(key)) {
-        keys.mnemonic = key
-      } else if (this.isKey(key)) {
-        if (this.watchOnly) keys.pub = key
-        else keys.priv = key
-      } else {
-        console.warn('Proceed called with invalid key', key)
-      }
-
-      this.$store.dispatch('initKeys', keys)
-      this.$store.dispatch('launch')
-      this.initializeKeys = false
-      // this.$router.push({
-      //   name: 'settings',
-      //   params: {showKeys: true}
-      // })
-      this.threads = []
-      this.eventsSet = new Set()
-      this.sub = pool.sub(
-        {
-          cb: async (event, relay) => {
-            switch (event.kind) {
-              case 0:
-                await this.$store.dispatch('addEvent', {event, relay})
-                return
-              case 1:
-              case 2:
-                if (this.eventsSet.has(event.id)) return
-                this.eventsSet.add(event.id)
-                addToThread(this.threads, event)
-                this.$store.state.homeFeed = this.threads
-                return
-            }
-          },
-          filter: [
-            {
-              authors: [
-                '7b0ba10b13233979d17e545d56b1c1f6563ce0c9b0d1f3691b5ad3bf3cced6c0'
-              ],
-              kinds: [0, 1, 3]
-            }
-          ]
-        },
-        'profile-browser'
-      )
-    },
-
-    isKey(key) {
-      return key?.toLowerCase()?.match(/^[0-9a-f]{64}$/)
-    }
-  }
 }
 </script>
