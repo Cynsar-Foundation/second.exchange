1adeb86e7754813b3c8003c19ca46f2be41dffed
Saransh Sharma <saransh@muellners.org>
2022-09-05 14:12:27 +0530

    Changes for Developer Ready

--- next.config.js
+++ next.config.js
@@ -6,6 +6,29 @@ const nextConfig = {
     path: "",
   },
   swcMinify: true,
+  env:{
+    local: true
+  },
+
+  publicRuntimeConfig: {
+    relays: {
+      local: [
+        'ws://localhost:8008'
+      ],
+      default: [
+      "wss://nostr-pub.wellorder.net",
+      "wss://nostr-relay.wlvs.space",
+      "wss://relay.damus.io",
+      "wss://nostr-pub.semisol.dev",
+      "wss://nostr.oxtr.dev",
+      "wss://nostr-relay.untethr.me"
+    ],
+      foundation: [
+        "wss://relay.cynsar.foundation"
+      ]
+    }
+  }
+  
 };
 
 module.exports = nextConfig;
